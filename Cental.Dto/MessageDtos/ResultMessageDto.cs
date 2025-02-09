0a65664a75b222542cc5eb93f74985447ad92122
ABOUBACAR <175620651+AboubacarSow@users.noreply.github.com>
2025-02-09 23:33:28 +0300

    Next step filter cars

--- Cental.Dto/MessageDtos/ResultMessageDto.cs
+++ Cental.Dto/MessageDtos/ResultMessageDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cental.DtoLayer.MessageDtos
+﻿namespace Cental.DtoLayer.MessageDtos
 {
     public record ResultMessageDto(int Id, string FullName, string Email, string Subject,string Body, bool IsRead);
     
--- Cental.WebUI/Controllers/DefaultController.cs
+++ Cental.WebUI/Controllers/DefaultController.cs
@@ -1,17 +1,26 @@
 ﻿using Cental.BusinessLayer.Abstract;
+using Cental.DataAccesLayer.Context;
+using Cental.DtoLayer.Enums.Car_Enums;
 using Cental.DtoLayer.MessageDtos;
 using Cental.EntityLayer.Entities;
+using Cental.WebUI.Infrastructure.Extensions;
 using Mapster;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Cental.WebUI.Controllers
 {
     [AllowAnonymous]
-    public class DefaultController(IMessageService _messageManager) : Controller
+    public class DefaultController(IMessageService _messageManager,
+                ICarService _carManager,CentalDbContext _context) : Controller
     {
         public IActionResult Index()
         {
+            
+
             return View();
         }
         public IActionResult Contact()
@@ -31,6 +40,57 @@ namespace Cental.WebUI.Controllers
             {
                 return Json(new { success = false, message = "Tekrar girdiğiniz bilgileri kontrol ediniz" });
             }
+        }
+        public IActionResult Cars()
+        {
+            var values = _carManager.TGetAll();
+            var data = TempData["FilteredCars"].ToString();
+            if (data != null)
+            {
+                values = JsonSerializer.Deserialize<List<Car>>(data, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+                });
+            }
+            return View(values);  
+        }
+        public PartialViewResult FilterCars()
+        {
+            
+            ViewBag.Cars = (from car in _carManager.TGetAll()
+                              select new SelectListItem
+                              {
+                                  Text = car.Brand.BrandName + " " + car.ModelName,
+                                  Value = car.Brand.BrandName + " " + car.ModelName
+                              }).ToList();
+
+            ViewBag.Models = (from model in _carManager.TGetAll()
+                              select new SelectListItem
+                              {
+                                  Text = model.ModelName,
+                                  Value = model.ModelName
+                              }).ToList();
+            ViewBag.Gastypes=GetEnumValues.GetValues<GasType>();
+            ViewBag.Geartypes=GetEnumValues.GetValues<GearType>();
+            return PartialView();
+
+        }
+        [HttpPost]
+        public IActionResult FilterCars(string car, string gear, int year, string gas)
+        {
+            if(string.IsNullOrEmpty(car) || string.IsNullOrEmpty(gear)|| string.IsNullOrEmpty(gas)|| year>0)
+            {
+                var values = _context.Cars.Where(c => c.Brand.BrandName + " " + c.ModelName == car
+                && c.GasType == gas
+                && c.GearType==gear
+                &&c.Year>=year).ToList();
+                TempData["FilteredCars"] = JsonSerializer.Serialize(values, new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.IgnoreCycles
+
+                });
+            }
+            return RedirectToAction("Cars");
         }
     }
 }
