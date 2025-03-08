c5c17e0abe7c79005c841a1408c10e6446a0f1f1
ABOUBACAR <175620651+AboubacarSow@users.noreply.github.com>
2025-03-08 19:32:36 +0300

    Updated Admin Dashboard

--- Cental.WebUI/Controllers/AdminDashboardController.cs
+++ Cental.WebUI/Controllers/AdminDashboardController.cs
@@ -1,14 +1,52 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using AutoMapper;
+using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.CarDtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cental.WebUI.Controllers
 {
     [Authorize(Roles ="Admin")]
-    public class AdminDashboardController : Controller
+    public class AdminDashboardController(IBrandService _brandService,
+        IBranchService _branchService,
+        IMessageService _messageSevice,
+        IServiceService _service,
+        ITestimonialService _testimonialService,
+        ICarService _carService,
+        IMapper _mapper) : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            GetTotalBrand();
+            GetTotalBranch();
+            GetTotalServices();
+            GetTotalMessages();
+            GetTotalRating();
+            var cars = _carService.TGetAll();
+            var carsDto = _mapper.Map<List<ResultListCarDto>>(cars)
+                .OrderByDescending(b => b.CarId)
+                .Take(6).ToList();
+            return View(carsDto);
+        }
+        private void GetTotalBrand()
+        {
+            ViewBag.Brands = _brandService.TGetAll().Count;
+        }
+        private void GetTotalBranch()
+        {
+            ViewBag.Branches = _branchService.TGetAll().Count;
+        }
+        private void GetTotalServices()
+        {
+            ViewBag.Services = _service.TGetAll().Count;
+        }
+        private void GetTotalMessages()
+        {
+            ViewBag.Emails = _messageSevice.TGetAll().Count;
+        }
+        private void GetTotalRating()
+        {
+            ViewBag.Rating = _testimonialService.TGetAll().Count;
         }
     }
 }
---
+++ Cental.WebUI/ViewComponents/DashBoard/_LocationComponent.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Cental.BusinessLayer.Abstract;
+using Cental.DtoLayer.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cental.WebUI.ViewComponents.DashBoard
+{
+    public class _LocationComponent(ICarService _carService,IMapper _mapper) :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var cars = _carService.TGetAll();
+            var carsDto = _mapper.Map<List<ResultListCarDto>>(cars)
+                .OrderByDescending(b => b.CarId)
+                .Take(6).ToList();
+            return View(carsDto);
+        }
+    }
+}
